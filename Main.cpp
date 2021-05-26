313450033c8b6de030735e1b1d7b2ebf748ecfe8
aloneshed <73320761+aloneshed@users.noreply.github.com>
2021-05-26 19:19:38 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+#include "BinaryTree.hpp"
+using namespace ariel;
+
+int main() {
+
+    BinaryTree<int> BT;
+    tree.add_root(10);
+    tree.add_right(10, 11);
+}
